43f6947c6f60552f9b0816ee2a6a95e96c4143d2
Oren Kravetz <40276134+okravetz@users.noreply.github.com>
2026-04-19 21:05:51 -0400

    update for subabase access

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __SB_URL__: JSON.stringify(process.env.VITE_SUPABASE_URL),
+    __SB_KEY__: JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
+  },
 })
